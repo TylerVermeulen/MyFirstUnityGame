6900da584c202c12b238b37ea51d1cf1276e92c0
Jannes <37171@ma-web.nl>
2024-03-12 15:45:26 +0100

    started map development

--- MySecondUnityGame/Assets/Scripts/Yoink.cs
+++ MySecondUnityGame/Assets/Scripts/Yoink.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Yoink : MonoBehaviour
 {
-    [SerializeField]private GameObject Gate1;
+    [SerializeField] private GameObject Gate1;
     public int Flower_Count = 0;
     // Start is called before the first frame update
     void Start()
@@ -15,28 +16,32 @@ public class Yoink : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyDown("o"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
-    
+
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown("g"))
         {
             if (other.tag == "Flower")
             {
-                Destroy(other.gameObject,1f);
-                Flower_Count ++;
+                Destroy(other.gameObject, 1f);
+                Flower_Count++;
             }
 
         }
         if (Input.GetKeyDown("t") && Flower_Count >= 2)
         {
-           if (other.tag == "Shop")
+            if (other.tag == "Shop")
             {
                 Debug.Log("toets");
                 Destroy(Gate1);
             }
         }
+
     }
 
 }
