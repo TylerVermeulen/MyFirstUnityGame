c39ad104d1682006232b18bfbf93ff6d1396b679
Jannes <37171@ma-web.nl>
2024-03-31 11:41:15 +0200

    (hopefully not) the last working copy of mapGen.cs
    going to make locationY dynamic now

--- MySecondUnityGame/Assets/Character/Scripts/mapGen.cs
+++ MySecondUnityGame/Assets/Character/Scripts/mapGen.cs
@@ -10,6 +10,24 @@ public class DoorModel
 	public Quaternion rot;
 	public bool isDoor;
 }
+class Room
+{
+	internal GameObject roomObject { get; set; }
+	internal bool doorNorth { get; set; }
+	internal bool doorEast { get; set; }
+	internal bool doorSouth { get; set; }
+	internal bool doorWest { get; set; }
+
+	public Room(GameObject roomObject)
+	{
+		this.roomObject = roomObject;
+		this.doorNorth = false;
+		this.doorEast = false;
+		this.doorSouth = false;
+		this.doorWest = false;
+	}
+}
+
 public class mapGen : MonoBehaviour
 {
 	[SerializeField] private int steps = 0;
@@ -74,70 +92,31 @@ public class mapGen : MonoBehaviour
 		for (int i = 0, L = rooms.Length; i < L; i++)
 		{
 			//begin north
-
-
-		//door.transform.position = noDoor.transform.position = new Vector3(
-		//	rooms[i].roomObject.transform.position.x + diameter / 2,
-		//	rooms[i].roomObject.transform.position.y,
-		//	rooms[i].roomObject.transform.position.z);
-
 			listOfDoorsToInstantiate.Add(
 				new DoorModel()
 				{
 
 					isDoor =rooms[i].doorNorth,
 					position=	new Vector3(rooms[i].roomObject.transform.position.x , rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z - diameter / 2),
-					rot=	Quaternion.Euler(0, 0, 0)
+					rot=	Quaternion.Euler(0, 90, 0)
 				}
-			);
-
-		//door.transform.position = noDoor.transform.position = new Vector3(rooms[i].roomObject.transform.position.x + diameter / 2, rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z);
-		//door.transform.rotation = noDoor.transform.rotation = Quaternion.Euler(0, 0, 0);
-		//if (rooms[i].doorNorth)
-		//{
-		//	listOfDoorsToInstantiate.Add(door);
-		//}
-		//else
-		//{
-		//	listOfDoorsToInstantiate.Add(noDoor);
-		//}
+			);		
 			//end north
 
 			//begin south
-			//door.transform.position = noDoor.transform.position = new Vector3(rooms[i].roomObject.transform.position.x - diameter / 2, rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z);
-			//door.transform.rotation = noDoor.transform.rotation = Quaternion.Euler(0, 180, 0);
-			//if (rooms[i].doorSouth)
-			//{
-			//	listOfDoorsToInstantiate.Add(door);
-			//}
-			//else
-			//{
-			//	listOfDoorsToInstantiate.Add(noDoor);
-			//}
-
-
 			listOfDoorsToInstantiate.Add(
 				new DoorModel()
 				{
 
 					isDoor = rooms[i].doorSouth,
 					position = new Vector3(rooms[i].roomObject.transform.position.x , rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z + diameter / 2),
-					rot = Quaternion.Euler(0, 180, 0)
+					rot = Quaternion.Euler(0, 270, 0)
 				}
 			);
 			//end south
 
 			//begin west
-		//door.transform.position = noDoor.transform.position = new Vector3(rooms[i].roomObject.transform.position.x, rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z + diameter / 2);
-		//door.transform.rotation = noDoor.transform.rotation = Quaternion.Euler(0, 270, 0);
-		//if (rooms[i].doorWest)
-		//{
-		//	listOfDoorsToInstantiate.Add(door);
-		//}
-		//else
-		//{
-		//	listOfDoorsToInstantiate.Add(Gam(noDoor));
-		//}
+		
 
 
 			listOfDoorsToInstantiate.Add(
@@ -146,43 +125,23 @@ public class mapGen : MonoBehaviour
 
 					isDoor = rooms[i].doorWest,
 					position = new Vector3(rooms[i].roomObject.transform.position.x - diameter / 2, rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z),
-					rot = Quaternion.Euler(0, 270, 0)
+					rot = Quaternion.Euler(0, 180, 0)
 				}
 			);
 			//end west
 
 			//begin east
-			//door.transform.position = noDoor.transform.position = new Vector3(rooms[i].roomObject.transform.position.x, rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z - diameter / 2);
-			//door.transform.rotation = noDoor.transform.rotation = Quaternion.Euler(0, 90, 0);
-			//if (rooms[i].doorEast)
-			//{
-			//	listOfDoorsToInstantiate.Add(door);
-			//}
-			//else
-			//{
-			//	listOfDoorsToInstantiate.Add(noDoor);
-			//}
 			listOfDoorsToInstantiate.Add(
 				new DoorModel()
 				{
 
 					isDoor = rooms[i].doorEast,
 					position = new Vector3(rooms[i].roomObject.transform.position.x + diameter / 2, rooms[i].roomObject.transform.position.y, rooms[i].roomObject.transform.position.z ),
-					rot = Quaternion.Euler(0, 90, 0)
+					rot = Quaternion.Euler(0, 0, 0)
 				}
 			);
 			//end east
-
-		//for (int h = 0; h < listOfDoorsToInstantiate.Count; h++)
-		//{
-		//	Debug.Log(i, listOfDoorsToInstantiate[i]);
-		//}
 		}
-
-
-		//List<Vector2> vectors = new List<Vector2>();
-
-	//	List<Vector2> newVectors = new List<Vector2>(vectors);
 
 		//remove duplicates      
 		//null the right duplicates
@@ -208,31 +167,7 @@ public class mapGen : MonoBehaviour
 		return listOfDoorsToInstantiate.Where(i => i != null)
 			.Select(i => DoorModelToGameObject(i))
 			.ToArray();
-		////actually remove duplicates
-		//List<GameObject> aaa = new List<GameObject>();
-		//
-		//foreach (GameObject gameObject in listOfDoorsToInstantiate)
-		//{
-		//	if (gameObject != null)
-		//	{
-		//		aaa.Add(gameObject);
-		//	}
-		//}
-		//
-		//
-		////for (int i = 0; i < aaa.Count; i++)
-		//{
-		//	//Debug.Log(aaa[i]);
-		//}
-		//
-		//// instantiate
-		//GameObject[] returnValue = new GameObject[aaa.Count];
-		//for (int i = 0; i < aaa.Count; i++)
-		//{
-		//	//Debug.Log($"returnValue   {i}");
-		//	 Instantiate(aaa[i]);
-		//}
-		//return aaa.ToArray();
+		
 
 
 	}
@@ -242,205 +177,10 @@ public class mapGen : MonoBehaviour
 		GameObject obj = 		Instantiate(model.isDoor ? door:noDoor);
 		obj.transform.position = model.position;
 		obj.transform.rotation = model.rot;
-		obj.transform.localScale = new Vector3(3, 3, 3);
+		//obj.transform.localScale = new Vector3(3, 3, 3);
 
 		return obj;
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	////remove duplicates
-	//Debug.Log($"remove  duplicates");
-	//for (int i = doorIndex - 1; i > 0; i--)
-	//{
-	//	Debug.Log($"remove  duplicates {i}");
-	//	for (int j = 1 + i; j != doorIndex; j++)
-	//	{
-	//		Debug.Log($"remove  duplicates {j}");
-	//		if (listOfDoorsToInstantiate[i] != null && listOfDoorsToInstantiate[j] != null)
-	//		{
-	//			if (listOfDoorsToInstantiate[i].transform.position == listOfDoorsToInstantiate[j].transform.position)
-	//			{
-	//				listOfDoorsToInstantiate[j] = null;
-	//			}
-	//		}
-	//	}
-	//}
-	//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//dunno if lists or arrays can be resized or added to + too lazy to figure out, this works 
-	int doorIndex = 0;
-
-	// The next part of this function assuems that when looking at the door prefab, the front(aka the blue arrow) points to the left
-	// The same should apply to the noDoor prefab
-
-	// now behold a fuckton of (--D--)ry-code
-
-
-
-	//foreach (GameObject item in path)
-	//{
-	//	Debug.Log($"item {item.name}");
-	//	// +x north, -x south, +z west, -z east
-	//
-	//	// summary: if next to me there exists another area, place door prefab in list, if not, placce noDoor prefab in list so hole of area prefab can later be patched
-	//	for (int i = 0; i < path.Length; i++)
-	//	{
-	//		Debug.Log($"path {i}");
-	//		//Debug.log90
-	//		// begin north
-	//		if (item.transform.position.x == (path[i].transform.position.x + diameter) && item.transform.position.z == path[i].transform.position.z)
-	//		{
-	//			door.transform.position = new Vector3(item.transform.position.x + diameter / 2, item.transform.position.y, item.transform.position.z);
-	//			door.transform.rotation = Quaternion.Euler(0, 0, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = door;
-	//			doorIndex++;
-	//		}
-	//		else
-	//		{
-	//			noDoor.transform.position = new Vector3(item.transform.position.x + diameter / 2, item.transform.position.y, item.transform.position.z);
-	//			noDoor.transform.rotation = Quaternion.Euler(0, 0, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = noDoor;
-	//			doorIndex++;
-	//		}
-	//		// end north
-	//
-	//
-	//		// begin south
-	//		if (item.transform.position.x == (path[i].transform.position.x - diameter) && item.transform.position.z == path[i].transform.position.z)
-	//		{
-	//			door.transform.position = new Vector3(item.transform.position.x - diameter / 2, item.transform.position.y, item.transform.position.z);
-	//			door.transform.rotation = Quaternion.Euler(0, 180, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = door;
-	//			doorIndex++;
-	//		}
-	//		else
-	//		{
-	//			noDoor.transform.position = new Vector3(item.transform.position.x - diameter / 2, item.transform.position.y, item.transform.position.z);
-	//			noDoor.transform.rotation = Quaternion.Euler(0, 180, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = noDoor;
-	//			doorIndex++;
-	//		}
-	//		// end south
-	//
-	//
-	//		// begin west
-	//		if (item.transform.position.x == path[i].transform.position.x && item.transform.position.z == (path[i].transform.position.z + diameter))
-	//		{
-	//			door.transform.position = new Vector3(item.transform.position.x, item.transform.position.y, item.transform.position.z + diameter / 2);
-	//			door.transform.rotation = Quaternion.Euler(0, 270, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = door;
-	//			doorIndex++;
-	//		}
-	//		else
-	//		{
-	//			noDoor.transform.position = new Vector3(item.transform.position.x, item.transform.position.y, item.transform.position.z + diameter / 2);
-	//			noDoor.transform.rotation = Quaternion.Euler(0, 270, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = noDoor;
-	//			doorIndex++;
-	//		}
-	//		// end west
-	//
-	//
-	//		// begin east
-	//		if (item.transform.position.x == path[i].transform.position.x && item.transform.position.z == (path[i].transform.position.z - diameter))
-	//		{
-	//			door.transform.position = new Vector3(item.transform.position.x, item.transform.position.y, item.transform.position.z - diameter / 2);
-	//			door.transform.rotation = Quaternion.Euler(0, 90, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = door;
-	//			doorIndex++;
-	//		}
-	//		else
-	//		{
-	//			noDoor.transform.position = new Vector3(item.transform.position.x, item.transform.position.y, item.transform.position.z - diameter / 2);
-	//			noDoor.transform.rotation = Quaternion.Euler(0, 90, 0);
-	//			listOfDoorsToInstantiate[doorIndex] = noDoor;
-	//			doorIndex++;
-	//		}
-	//		// end west
-	//	}
-	//}
-	//
-	////remove duplicates
-	//Debug.Log($"remove  duplicates");
-	//for (int i = doorIndex - 1; i > 0; i--)
-	//{
-	//	Debug.Log($"remove  duplicates {i}");
-	//	for (int j = 1 + i; j != doorIndex; j++)
-	//	{
-	//		Debug.Log($"remove  duplicates {j}");
-	//		if (listOfDoorsToInstantiate[i] != null && listOfDoorsToInstantiate[j] != null)
-	//		{
-	//			if (listOfDoorsToInstantiate[i].transform.position == listOfDoorsToInstantiate[j].transform.position)
-	//			{
-	//				listOfDoorsToInstantiate[j] = null;
-	//			}
-	//		}
-	//	}
-	//}
-	//
-	//// instantiate
-	//GameObject[] returnValue = new GameObject[int.MaxValue / 1024];
-	//for (int i = 0; i < doorIndex; i++)
-	//{
-	//	Debug.Log($"returnValue   {i}");
-	//	returnValue[i] = Instantiate(listOfDoorsToInstantiate[i]);
-	//}
-	//return  returnValue;
-	//
 
 
 	private GameObject[] genMap(GameObject[] path)
@@ -516,50 +256,4 @@ public class mapGen : MonoBehaviour
 		return path;
 	}
 }
-// if (item.transform.position.x == (path[i].transform.position.x - diameter) && item.transform.position.z == path[i].transform.position.z)
-// {
 
-// }
-// if (item.transform.position.x == path[i].transform.position.x && item.transform.position.z == (path[i].transform.position.z + diameter))
-// {
-
-// }
-// if (item.transform.position.x == path[i].transform.position.x && item.transform.position.z == (path[i].transform.position.z + diameter))
-// {
-
-// }
-
-
-
-class Tform 
-{
-	internal Vector3 pos { get; set; } 
-	internal Quaternion rot { get; set; }
-	public Tform( Vector3 pos, Quaternion rot)
-	{
-		this.pos = pos;	
-		this.rot = rot;
-	}
-	
-	public void clone()
-	{
-
-	}
-}
-class Room
-{
-	internal GameObject roomObject { get; set; }
-	internal bool doorNorth { get; set; }
-	internal bool doorEast { get; set; }
-	internal bool doorSouth { get; set; }
-	internal bool doorWest { get; set; }
-
-	public Room(GameObject roomObject)
-	{
-		this.roomObject = roomObject;
-		this.doorNorth = false;
-		this.doorEast = false;
-		this.doorSouth = false;
-		this.doorWest = false;
-	}
-}
