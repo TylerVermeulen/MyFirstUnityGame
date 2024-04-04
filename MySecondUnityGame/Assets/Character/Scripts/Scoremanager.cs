1a28761e1af92b788119016ca7ed817a350a77fa
TylerVermeulen <36793@ma-web.nl>
2024-04-04 11:26:32 +0200

    ui scores

--- MySecondUnityGame/Assets/Character/Scripts/Scoremanager.cs
+++ MySecondUnityGame/Assets/Character/Scripts/Scoremanager.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
+
 
 public class Scoremanager : MonoBehaviour
 {
-    public Text Flowercounttext;
+    public static Scoremanager instance;
+    public TMP_Text Flowercounttext;
     int Flowercount = 0;
+
+
+    private void Awake()
+    {
+        instance = this; 
+    }
     // Start is called before the first frame update
     void Start()
     {
-        Flowercounttext.text = Flowercount.ToString() + "POINTS";
+        Flowercounttext.text = Flowercount.ToString() + " flowers";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void AddPoint()
+    {
+        Flowercount += 1;
+        Flowercounttext.text = Flowercount.ToString() + " flowers";
+    }
+    public void RemovePoint()
+    {
+        Flowercount -= 14;
     }
 }
